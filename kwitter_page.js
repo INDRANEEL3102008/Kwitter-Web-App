055468c553639b6d8660e063f75bd5dc4e71adc3
INDRANEEL3102008 <77006195+INDRANEEL3102008@users.noreply.github.com>
2021-02-23 19:14:13 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -61,5 +61,5 @@ function updateLike(message_id) {
 function logout() {
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
-      window.location.replace("Index.html");
+      window.location.replace("index.html");
 }
